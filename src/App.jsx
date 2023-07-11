aafdbad5c283eb3afcbaee04dec0021d4d4a7bcb
Christiano0407 <placebo0704@gmail.com>
2023-07-11 12:54:58 -0600

    first commit of E-commerce

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main>
+        <div>React App</div>
+      </main>
+    </>
+  )
+}
+
+export default App
