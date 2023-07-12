dc7ce528d8b10a3bb0bbab5a309dd56b8b4976b7
Christiano0407 <placebo0704@gmail.com>
2023-07-11 20:50:21 -0600

    routes add pages

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,23 +1,34 @@
-//** ==== Modules Imports  ====  */
+//** ==== Router DOM  Import ====  */
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+//** ==== Modules Imports & Components  ====  */
 import { Home } from '../Home'
 import { MyAccount } from '../MyAccount/index'
 import { MyOrder } from '../MyOrder/index'
 import { MyOrders } from '../MyOrders/index'
+import { NotFound } from '../NotFound/index'
 import { SignIn } from '../SignIn/index'
-
 //import { useState } from 'react'
 import '../App/App.css'
+
+//** === Routes ===  */
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/*', element: <NotFound /> },
+    { path: '/sign-in', element: <SignIn /> },
+  ])
+  return routes
+}
 
 //** === APP === */
 function App() {
   return (
-    <main className="">
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <SignIn />
-    </main>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
